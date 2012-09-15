bab4f68759094fee0cfbed1a0a928deb622ae403
Christian Sterzl <christian.sterzl@gmail.com>
2012-09-15 22:22:44 +0200

    Tilt added

--- new/js/script.js
+++ new/js/script.js
@@ -1,5 +1,7 @@
 $(window).bind('hashchange', function() {
   
-  $('a[href=' + window.location.hash + ']').addClass('active');
+  var link = $('a[href=' + window.location.hash + ']');
+  link.addClass('active');
+  link.parent().addClass('active');
 
 });
