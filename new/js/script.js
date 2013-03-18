410177945ce07d212ec138d36ea66ddf341625e8
Waxolunist <christian.sterzl@gmail.com>
2013-03-18 10:55:22 +0100

    onclick handler instead of hashchange

--- new/js/script.js
+++ new/js/script.js
@@ -1,11 +1,10 @@
-$(window).bind('hashchange', function() {
-  
-  var link = $('a[href=' + window.location.hash + ']');
-  link.addClass('active');
-  link.parent().addClass('active');
-
+/*$(window).bind('hashchange', function() {
+});*/
+$(document).ready(function() {
+  $('nav > a').click(function(){
+    $('nav > a').removeClass('active');
+    var link = $(this);
+    link.addClass('active');
+    link.parent().addClass('active');
+  });
 });
-$('nav > a').click(function(){
-  
-});
-//TODO add onclick event to, not only on hashchange
